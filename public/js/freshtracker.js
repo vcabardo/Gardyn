a8b6d82959dc919a0dcdee1d74199e231a7c1761
vcabardo <42554910+vcabardo@users.noreply.github.com>
2020-11-29 23:56:04 -0700

    Changes to search and freshtrack
    Search now displays all available items for the user as well as a list of all of the items that the user has collected. Freshtrack now displays the number of days remaining until a product expires

--- public/js/freshtracker.js
+++ public/js/freshtracker.js
@@ -1,6 +1,5 @@
 //Populate the DOM with a dynamic list of Bootstrap card elements,
 //with data taken from the RTDB
-
 function chooseCategory(input) {
   if (input == 1) return "Fruits & Veggies";
   else if (input == 2) return "Meat & Protein";
@@ -12,7 +11,6 @@ function chooseCategory(input) {
 }
 
 function getAllElementsOfChildFT(childName) {
-
   var config = {
     apiKey: " AIzaSyAxfhLzaQgDEY-QFO8dc7LZ2aQTXc2fg3k ",
     authDomin: "gardynapp.firebaseapp.com",
@@ -24,119 +22,132 @@ function getAllElementsOfChildFT(childName) {
     firebase.initializeApp(config);
   }
 
-  var db = firebase.database();
-  var ref = db.ref(childName);
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      var db = firebase.database();
+      var ref = db.ref('Users/' + user.displayName + '/Collected/');
 
-  //TODO: Access the fresh tracker on a user basis
-  ref.on("value", function (snapshot) {
-    snapshot.forEach((child) => {
-      var pE = child.val().productExperationDate;
-      var pN = child.val().productName;
-      var pC = child.val().productCategory;
-      var pP = child.val().productPurchaseDate;
-      var pNotes = child.val().productInfo;
+      ref.on("value", function (snapshot) {
+        snapshot.forEach((child) => {
+          var pE = child.val().productExperationDate;
+          var pN = child.val().productName;
+          var pC = child.val().productCategory;
+          var pP = child.val().productPurchaseDate;
+          var pNotes = child.val().productInfo;
 
-      if (pN != undefined) {
-        var currentDate = new Date();
-        var expirationDate = new Date(
-          parseInt(pE.substring(6, 10)),
-          parseInt(pE.substring(0, 2)) - 1,
-          parseInt(pE.substring(3, 5)));
+            if(pN != undefined){
+              var currentDate = new Date();
+              var expirationDate = new Date(
+                parseInt(pE.substring(6, 10)),
+                parseInt(pE.substring(0, 2)) - 1,
+                parseInt(pE.substring(3, 5)));
 
-        var purchaseDate = new Date(
-          parseInt(pP.substring(6, 10)),
-          parseInt(pP.substring(0, 2)) - 1,
-          parseInt(pP.substring(3, 5)));
+              var purchaseDate = new Date(
+                parseInt(pP.substring(6, 10)),
+                parseInt(pP.substring(0, 2)) - 1,
+                parseInt(pP.substring(3, 5)));
 
-        const DAY_IN_MILLI = 1000 * 60 * 60 * 24
-        var timeUntilExpiration = Math.floor(Math.abs(expirationDate - currentDate) / DAY_IN_MILLI);
-        var totalTime = Math.floor(Math.abs(expirationDate - purchaseDate) / DAY_IN_MILLI);
+              const DAY_IN_MILLI = 1000 * 60 * 60 * 24;
+              var timeUntilExpiration = Math.floor(Math.abs(expirationDate - currentDate) / DAY_IN_MILLI);
+              var totalTime = Math.floor(Math.abs(expirationDate - purchaseDate) / DAY_IN_MILLI);
 
-        var progress = document.createElement("div");
-        progress.classList.add("progress");
+              var progress = document.createElement("div");
+              progress.classList.add("progress");
 
-        var progressBar = document.createElement("div");
-        progressBar.classList.add("progress-bar");
-        progressBar.setAttribute("role", "progressbar");
-        progressBar.setAttribute("aria-valuemin", 0);
-        progressBar.setAttribute("aria-valuemax", totalTime);
-        progressBar.setAttribute("aria-valuenow", 50);
-        progressBar.setAttribute('style', 'width:' + ((timeUntilExpiration / totalTime) * 100) + '%');
-        var progressLabel = document.createTextNode(timeUntilExpiration + " / " + totalTime + " Days Left");
-        progressBar.appendChild(progressLabel);
+              var progressBar = document.createElement("div");
+              progressBar.classList.add("progress-bar");
+              progressBar.setAttribute("role", "progressbar");
+              progressBar.setAttribute("aria-valuemin", 0);
+              progressBar.setAttribute("aria-valuemax", totalTime);
+              progressBar.setAttribute("aria-valuenow", 50);
+              progressBar.setAttribute('style','width:'+ ((timeUntilExpiration / totalTime) * 100) +'%');
 
-        if ((timeUntilExpiration / totalTime) <= 0.33) {
-          progressBar.classList.add("bg-danger");
-        } else if ((timeUntilExpiration / totalTime) <= 0.7) {
-          progressBar.classList.add("bg-warning");
-        } else if ((timeUntilExpiration / totalTime) < 1) {
-          progressBar.classList.add("bg-success");
-        }
+              if(timeUntilExpiration <= 3) {
+                progressBar.classList.add("bg-danger");
+              } else if (timeUntilExpiration <= 7) {
+                progressBar.classList.add("bg-warning");
+              } else {
+                progressBar.classList.add("bg-success");
+              }
 
-        var card = document.createElement("div");
-        card.classList.add("card");
-        card.classList.add("bg-secondary");
+              var card = document.createElement("div");
+              card.classList.add("card");
+              card.classList.add("bg-secondary");
 
-        var cardHeader = document.createElement("div");
-        cardHeader.classList.add("card-header");
-        var name = document.createTextNode(pN);
-        cardHeader.appendChild(name);
-        progress.appendChild(progressBar);
-        cardHeader.appendChild(progress);
-        cardHeader.classList.add("bg-secondary");
-        card.classList.add("text-light");
+              var cardHeader = document.createElement("div");
+              cardHeader.classList.add("card-header");
+              var name = document.createTextNode(pN);
+              cardHeader.appendChild(name);
+              cardHeader.classList.add("bg-secondary");
+              card.classList.add("text-light");
 
-        var cardBody = document.createElement("div");
-        cardBody.classList.add("card-body");
-        cardBody.classList.add("bg-secondary");
-        cardBody.classList.add("text-light");
+              var cardBody = document.createElement("div");
+              cardBody.classList.add("card-body");
+              cardBody.classList.add("bg-secondary");
+              cardBody.classList.add("text-light");
 
-        var cardFooter = document.createElement("div");
-        cardFooter.classList.add("card-footer");
+              progress.appendChild(progressBar);
+              cardBody.appendChild(progress);
 
-        var node = document.createTextNode("Product Category: " + chooseCategory(pC) +
-          "\nPurchase Date: " + pP +
-          "\nExpiration Date: " + pE +
-          "\nNotes: " + pNotes
-        );
+              var cardFooter = document.createElement("div");
+              cardFooter.classList.add("card-footer");
+              cardFooter.classList.add("bg-secondary");
+              cardFooter.classList.add("container");
 
-        cardBody.appendChild(node);
+              var expirationDateDiv = document.createElement("div");
+              expirationDateDiv.classList.add("text-center");
+              var expirationDateNode = document.createTextNode(timeUntilExpiration + " Days Left!");
+              expirationDateDiv.appendChild(expirationDateNode);
+              cardBody.appendChild(expirationDateDiv);
 
-        //TODO: Access the freshtracker for a particular user and mark
-        //entries as thrown away or used
-        var usedButton = document.createElement("BUTTON");
-        usedButton.innerHTML = "Use";
-        usedButton.onclick = function () {
-             let dref = db.ref('addItem/' + pN);
-             dref.remove()
-             location.reload();
-        };
+              //TODO: Access the freshtracker for a particular user and mark
+              //entries as thrown away or used - add to stat page
+              var usedButton = document.createElement("BUTTON");
+              usedButton.innerHTML = "Use";
+              usedButton.onclick = function () {
+                   let dref = db.ref('addItem/' + pN);
+                   dref.remove()
+                   location.reload();
+              };
 
-        var throwawayButton = document.createElement("BUTTON");
-        throwawayButton.innerHTML = "Thrown Away";
-        throwawayButton.onclick = function () {
-             let dref = db.ref('addItem/' + pN);
-             dref.remove()
-             location.reload();
-        };
+              usedButton.classList.add("col-sm-6");
+              usedButton.classList.add("btn");
+              usedButton.classList.add("btn-light");
 
-        //TODO: space buttons evenly and style buttons
-        cardFooter.appendChild(usedButton);
-        cardFooter.appendChild(throwawayButton);
+              var throwawayButton = document.createElement("BUTTON");
+              throwawayButton.innerHTML = "Thrown Away";
+              throwawayButton.onclick = function () {
+                   let dref = db.ref('addItem/' + pN);
+                   dref.remove()
+                   location.reload();
+              };
 
-        card.appendChild(cardHeader);
-        card.appendChild(cardBody);
-        card.appendChild(cardFooter);
+              throwawayButton.classList.add("col-sm-6");
+              throwawayButton.classList.add("btn");
+              throwawayButton.classList.add("btn-light");
 
-        var entry = document.createElement("div");
-        entry.classList.add("col-lg-6");
-        entry.classList.add("my-sm-3");
-        entry.appendChild(card);
+              //TODO: space buttons evenly and style buttons
+              cardFooter.appendChild(usedButton);
+              cardFooter.appendChild(throwawayButton);
 
-        var element = document.getElementById("d1");
-        element.appendChild(entry);
-      } //end if
-    });
+              card.appendChild(cardHeader);
+              card.appendChild(cardBody);
+              card.appendChild(cardFooter);
+
+              var entry = document.createElement("div");
+              entry.classList.add("col-lg-6");
+              entry.classList.add("my-sm-3");
+              entry.appendChild(card);
+
+              var element = document.getElementById("d1");
+              element.appendChild(entry);
+          }//end if
+        });
+      });
+
+    } else {
+      // No user is signed in.
+    }
   });
 
   // Attach an asynchronous callback to read the data at our posts reference
@@ -146,6 +157,47 @@ function getAllElementsOfChildFT(childName) {
     console.log("The read failed: " + errorObject.code);
   });
 
+}
+
+function writeUserData() {
+    var pName = document.getElementById("productName").value;
+    var pCat = document.getElementById("productCategory").value;
+    var pPurDate = document.getElementById("datePurchased").value;
+    var pExpDate = document.getElementById("dateExpire").value;
+    var pInfo = document.getElementById("productInfo").value;
+
+    var config = {
+      apiKey: " AIzaSyAxfhLzaQgDEY-QFO8dc7LZ2aQTXc2fg3k ",
+      authDomin: "gardynapp.firebaseapp.com",
+      databaseURL: "https://gardynapp.firebaseio.com/",
+      storageBucket: "gardynapp.appspot.com"
+    };
+
+    if (!firebase.apps.length) {
+      firebase.initializeApp(config);
+    }
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        firebase.database().ref('Users/' + user.displayName + '/Collected/' + pName).set({
+          productName: pName,
+          productCategory: pCat,
+          productPurchaseDate: pPurDate,
+          productExperationDate: pExpDate,
+          productInfo: pInfo
+        }, (error) => {
+          if (error) {
+            console.log("Error: Did not insert into database.");
+          } else {
+            console.log("Success");
+            resetForm();
+          }
+        });
+      } else {
+        // No user is signed in.
+      }
+    });
 }
 
 getAllElementsOfChildFT("addItem/");
--- public/js/search.js
+++ public/js/search.js
@@ -1,7 +1,4 @@
-
-
 function writeUserData() {
-
     var pName = document.getElementById("productName").value;
     var pCat = document.getElementById("productCategory").value;
     var pPurDate = document.getElementById("datePurchased").value;
@@ -19,21 +16,41 @@ function writeUserData() {
       firebase.initializeApp(config);
     }
 
-      firebase.database().ref('addItem/' + pName).set({
-        productName: pName,
-        productCategory: pCat,
-        productPurchaseDate: pPurDate,
-        productExperationDate: pExpDate,
-        productInfo: pInfo
-      }, (error) => {
-        if (error) {
-          console.log("Error: Did not insert into database.");
-        } else {
-          console.log("Success");
-          resetForm();
-        }
-      });
-    
+    firebase.database().ref('AllItems').push({
+      Name: pName,
+      ShelfLife: 5
+    }, (error) => {
+      if (error) {
+        console.log("Error: Did not insert into database.");
+      } else {
+        console.log("Success");
+        resetForm();
+      }
+    });
+
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        firebase.database().ref('Users/' + user.displayName + '/Collected/' + pName).set({
+          productName: pName,
+          productCategory: pCat,
+          productPurchaseDate: pPurDate,
+          productExperationDate: pExpDate,
+          productInfo: pInfo
+        }, (error) => {
+          if (error) {
+            console.log("Error: Did not insert into database.");
+          } else {
+            console.log("Success");
+            resetForm();
+          }
+        });
+      } else {
+        // No user is signed in.
+      }
+    });
+
+
 }
 
 function resetForm(){
@@ -53,8 +70,7 @@ function chooseCategory(input){
   else return "null";
 }
 
-function getAllElementsOfChild(childName) {
-
+function getAllElementsOfChild_myList(childName, id) {
   var config = {
     apiKey: " AIzaSyAxfhLzaQgDEY-QFO8dc7LZ2aQTXc2fg3k ",
     authDomin: "gardynapp.firebaseapp.com",
@@ -69,7 +85,6 @@ function getAllElementsOfChild(childName) {
   var db = firebase.database();
   var ref = db.ref(childName);
 
-  //TODO: Access the fresh tracker on a user basis
   ref.on("value", function (snapshot) {
     snapshot.forEach((child) => {
       var pE = child.val().productExperationDate;
@@ -86,7 +101,24 @@ function getAllElementsOfChild(childName) {
           var cardHeader = document.createElement("div");
           cardHeader.classList.add("card-header");
           var name = document.createTextNode(pN);
+
+          var addButton = document.createElement("BUTTON");
+          addButton.innerHTML = "+";
+          addButton.onclick = function () {
+            //TODO: set a listener to add the entry to the users list
+            //TODO: style button
+          };
+
+          var removeButton = document.createElement("BUTTON");
+          removeButton.innerHTML = "-";
+          removeButton.onclick = function () {
+            //TODO: set a listener to remove the entry from the users list
+            //TODO: style button
+          };
+
           cardHeader.appendChild(name);
+          cardHeader.appendChild(addButton);
+          cardHeader.appendChild(removeButton);
           cardHeader.classList.add("bg-secondary");
           card.classList.add("text-light");
 
@@ -103,11 +135,11 @@ function getAllElementsOfChild(childName) {
           card.appendChild(cardBody);
 
           var entry = document.createElement("div");
-          entry.classList.add("col-lg-6");
+          entry.classList.add("col-12");
           entry.classList.add("my-sm-3");
           entry.appendChild(card);
 
-          var element = document.getElementById("d1");
+          var element = document.getElementById(id);
           element.appendChild(entry);
       }//end if
     });
@@ -119,7 +151,89 @@ function getAllElementsOfChild(childName) {
   }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
-
 }
 
-getAllElementsOfChild("addItem/");
+function getAllElementsOfChild_search(childName, id) {
+  var config = {
+    apiKey: " AIzaSyAxfhLzaQgDEY-QFO8dc7LZ2aQTXc2fg3k ",
+    authDomin: "gardynapp.firebaseapp.com",
+    databaseURL: "https://gardynapp.firebaseio.com/",
+    storageBucket: "gardynapp.appspot.com"
+  };
+
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+
+  var db = firebase.database();
+  var ref = db.ref(childName);
+
+  ref.on("value", function (snapshot) {
+    snapshot.forEach((child) => {
+      var name = child.val().Name;
+      var shelfLife = child.val().ShelfLife;
+
+        if(name != undefined){
+          var card = document.createElement("div");
+          card.classList.add("card");
+          card.classList.add("bg-secondary");
+
+          var cardHeader = document.createElement("div");
+          cardHeader.classList.add("card-header");
+          var nameText = document.createTextNode(name);
+          var addButton = document.createElement("BUTTON");
+          addButton.innerHTML = "+";
+          addButton.onclick = function () {
+            //TODO: set a listener to add the entry to the users list
+            //TODO: style button
+          };
+
+          cardHeader.appendChild(addButton);
+          cardHeader.appendChild(nameText);
+
+          cardHeader.classList.add("bg-secondary");
+          card.classList.add("text-light");
+
+          var cardBody = document.createElement("div");
+          cardBody.classList.add("card-body");
+          cardBody.classList.add("bg-secondary");
+          cardBody.classList.add("text-light");
+
+          var node = document.createTextNode( "Shelf Life: " + shelfLife + " Days");
+
+          cardBody.appendChild(node);
+
+          card.appendChild(cardHeader);
+          card.appendChild(cardBody);
+
+          var entry = document.createElement("div");
+          entry.classList.add("col-lg-6");
+          entry.classList.add("my-sm-3");
+          entry.appendChild(card);
+
+          var element = document.getElementById(id);
+          element.appendChild(entry);
+      }//end if
+    });
+  });
+
+  // Attach an asynchronous callback to read the data at our posts reference
+  ref.on("value", function (snapshot) {
+    console.log(snapshot.val());
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
+}
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    getAllElementsOfChild_myList('Users/' + user.displayName + '/Collected/', "d1");
+  } else {
+    // No user is signed in.
+  }
+});
+
+//TODO: Write the function for searching the list
+
+getAllElementsOfChild_search("AllItems/", "allitems");
